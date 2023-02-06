c9c2ab5bf87c6a440f134773ab6852e1df7b4ed9
Dirk Arnez <smalldirkalex@gmail.com>
2023-02-07 03:27:59 +0800

    - update start.cmd

--- start.cmd
+++ start.cmd
@@ -1,6 +1,13 @@
 @echo off
 
-set USERPROFILE=%~dp0\USERPROFILE\
-set LOCALAPPDATA=%~dp0\LOCALAPPDATA\
+set USERPROFILE=%~dp0\USERPROFILE
+set LOCALAPPDATA=%~dp0\LOCALAPPDATA
+
+if not exist %LOCALAPPDATA% (
+	mkdir %LOCALAPPDATA%
+)
+if not exist %LOCALAPPDATA% (
+	mkdir %LOCALAPPDATA%\AppData\Roaming\Figma
+)
 
 start Figma.exe
